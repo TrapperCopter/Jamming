cb116b330b57c7afb36a2c4066c36f6ffe85ae11
Stephen <TF2Man999@gmail.com>
2018-05-16 23:02:23 -0400

    step 87

--- jamming/src/util/Spotify.js
+++ jamming/src/util/Spotify.js
@@ -1,19 +1,35 @@
-useraccesstoken =
-Spotify () {
-  let accessToken;
-  const Spotify = {
-    getAccessToken() {
-      if(accessTokenMatch && expiresInMatch)
-        return accessToken;
+let accessToken;
+const cliendID= "e0856715edfc4954b180a6d7ef15d93c";
+const redirectURL = "http://localhost:3000/";
+
+const Spotify = {
+
+  search(term) {
+    return fetch(
+      "https://api.spotify.com/v1/search?type=track&q=term" ,
+      {
+        headers: {Authorization: `Bearer ${accessToken}`}
       }
-      else {
-        window.location.href.match(
-          const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-          const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
-        )
-      }
+    )
+  }
+
+
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    if (accessTokenMatch && expiresInMatch) {
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      window.location("https://accounts.spotify.com/authorize?client_id=" + {cliendID} +
+      "&response_type=token&scope=playlist-modify-public&redirect_uri=" + {REDIRECT_URI})
+    } else {
+
     }
   }
-};
+}
 
 export default Spotify
