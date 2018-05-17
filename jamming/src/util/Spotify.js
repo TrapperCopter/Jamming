1e683051ba4c0694f78673391357aa752e97db53
Stephen <TF2Man999@gmail.com>
2018-05-16 23:37:52 -0400

    step 86

--- jamming/src/util/Spotify.js
+++ jamming/src/util/Spotify.js
@@ -6,13 +6,13 @@ const Spotify = {
 
   search(term) {
     return fetch(
-      .json("https://api.spotify.com/v1/search?type=track&q=term").map([
+      .json('https://api.spotify.com/v1/search?type=track&q=${term}').map([
         ID: track.id ,
         Name: track.name ,
         Artist: track.artist[0].name ,
         Album: track.album.name ,
         URI: track.uri
-      ]) ,
+      ]).then() ,
       {
         headers: {Authorization: `Bearer ${accessToken}`}
       }
