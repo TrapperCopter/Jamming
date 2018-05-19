6dd161a852e96de6c2a45a11b474b52b7b5d96ed
Stephen <TF2Man999@gmail.com>
2018-05-18 20:18:35 -0400

    fixed issue in playlist

--- jamming/src/Components/Playlist/Playlist.js
+++ jamming/src/Components/Playlist/Playlist.js
@@ -4,27 +4,29 @@ import './Playlist.css';
 
 class Playlist extends Component {
   constructor(props) {
-  super(props)
-  this.handleNameChange = this.handleNameChange.bind(this)
-}
-  handleNameChange(){
+    super(props)
+    this.handleNameChange = this.handleNameChange.bind(this)
+  }
 
+  handleNameChange(){
   }
 
   render() {
+    const tracklistProps = (
+      <div>
+        <TrackList tracks = {this.props.playlistTracks} />
+        <TrackList onAdd = {this.props.onAdd} />
+        <TrackList onRemove = {this.props.onRemove} />
+      </div>
+    );
     return (
       <div className="Playlist">
-      <input defaultValue={'New Playlist'} onChange={this.handleNameChange}/>
-      <TrackList />
-      <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
-</div>
+        <input defaultValue={'New Playlist'} onChange={this.handleNameChange}/>
+        <TrackList />
+        <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
+      </div>
     );
   }
-  const tracklistProps = (
-    <TrackList tracks = {this.props.playlistTracks} />
-    <TrackList onAdd = {this.props.onAdd} />
-    <TrackList onRemove = {this.props.onRemove} />
-  );
 }
 
 export default Playlist;
