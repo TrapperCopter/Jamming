1cd88c9fee97688d4546981827a7a77499c7b5d4
Stephen <TF2Man999@gmail.com>
2018-05-18 22:06:44 -0400

    map test

--- jamming/src/Components/SearchResults/SearchResults.js
+++ jamming/src/Components/SearchResults/SearchResults.js
@@ -14,12 +14,12 @@ class SearchResults extends Component {
       constructor(props) {
         super(props)
         this.state.searchResults = {
-          tracks: TrackList.map({
+          tracks: TrackList.map() => {
             track.id: tracks ,
             this.props.track.name: name ,
             this.props.track.artist: artist ,
             this.props.track.album: album
-          })
+          }
       }
     }
 }
